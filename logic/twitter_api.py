a9fc4bafbafeadf426512fe3544c200dc5807cf0
jbrown400 <justin.brown400@gmail.com>
2020-07-13 20:51:40 -0500

    Added more tweets. Figured out how to handle hashtags in the url. I had to properly encode the string.

--- logic/twitter_api.py
+++ logic/twitter_api.py
@@ -1,9 +1,11 @@
 import requests
+from urllib.parse import urlencode, quote_plus
 from requests_oauthlib import OAuth1
 from .constants import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
 
 def send_tweet(tweet_text: str):
-	url = f'https://api.twitter.com/1.1/statuses/update.json?status={tweet_text}'
+	url = f'https://api.twitter.com/1.1/statuses/update.json'
+	params = {'status': f'{tweet_text} #Reminders'}
 	auth = OAuth1(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-	response = requests.post(url, auth=auth)
+	requests.post(url, params=params, auth=auth)
